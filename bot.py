cd9911564aa71456df18843b3ceeaa40166e3474
Emil Kovacev <emilkova@buffalo.edu>
2021-06-07 10:54:24 -0400

    completed API request functionality for on_message, on_message_edit, on_message_delete

--- bot.py
+++ bot.py
@@ -1,5 +1,4 @@
 import discord
-
 import datetime
 from dotenv import load_dotenv
 import os
@@ -7,51 +6,78 @@ import requests
 
 # initializers
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
-GUILD_IDS = [int(os.getenv('GUILD_IDS',0))]
+DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+ANNOUNCEMENTS_ID = os.getenv('ANNOUNCEMENTS_ID')
+ANNOUNCEMENTS_TOKEN = os.getenv('ANNOUNCEMENTS_TOKEN')
+head = {'Authorization': 'Token ' + ANNOUNCEMENTS_TOKEN}
 
 intents = discord.Intents.all()
 intents.members = True
 
-bot = commands.Bot(command_prefix=COMMAND_PREFIX, description=DESCRIPTION,intents=intents)
+client = discord.Client()
 
 
 ###################################
 # database manipulation functions #
 ###################################
 
+
 # ensure that announcements database reflects discord channel with routine checks
-def sync_annountements():
+def sync_announcements():
+    # channel = client.get_channel(ANNOUNCEMENTS_ID)
+    # messages = channel.history(limit=200).flatten()
     pass
 
 
 # add new announcement to database
 def add_announcement(message):
-    pass
+    new_announcement = {
+        'text': message.content,
+        'discord_id': message.id,
+        'created_at': message.created_at
+    }
+    response = requests.post('http://127.0.0.1:8000/announcements/', new_announcement, headers=head)
+    return response.status_code
 
 
 # edit announcement to reflect discord channel
 def edit_announcement(message):
-    pass
+    edited_announcement = {
+        'text': message.content,
+        'discord_id': message.id,
+        'created_at': message.created_at
+    }
+    response = requests.patch(f'http://127.0.0.1:8000/announcements/{message.id}/', edited_announcement, headers=head)
+    return response.status_code
 
 
 # remove announcement
 def remove_announcement(message):
-    pass
+    response = requests.delete(f'http://127.0.0.1:8000/announcements/{message.id}/', headers=head)
+    return response.status_code
 
 
-@bot.event
-async def onReady():
+
+@client.event
+async def on_ready():
+    print('logged in as {0.user}'.format(client))
     sync_announcements()
+    print('announcements synced')
 
-@bot.event
+@client.event
 async def on_message(message):
+    print(message.content)
+    print(message.id)
+    print(message.created_at)
     add_announcement(message)
 
-@bot.event
-async def on_message_edit(message):
-    edit_announcement(message)
+@client.event
+async def on_message_edit(message_before, message_after):
+    edit_announcement(message_after)
 
-@bot.event
+@client.event
 async def on_message_delete(message):
     remove_announcement(message)
+
+
+client.run(DISCORD_TOKEN)
